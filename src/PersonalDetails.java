98811e31170a0a95ee5853e94c24d1fe7a72cc4c
shanikathiriya <krishna.sanghani1210@gmail.com>
2021-10-16 17:29:44 +0100

    test2 commit

---
+++ src/PersonalDetails.java
@@ -0,0 +1,20 @@
+public class PersonalDetails
+{
+    public static void main(String args[])
+    {
+        //Declaration of variable and initialize value
+        int salary=1500;
+        double percentage=75.57;
+        String name="Krishna";
+        String Address="31 Launceston Garden UB6 7EY";
+
+        // Print all the personal detail
+        System.out.println("My Name Is "+name);
+        System.out.println("I am Leaving At "+Address);
+        System.out.println("My Salary is "+salary);
+        System.out.println("My 12th standard Percentage is "+percentage);
+
+
+
+    }
+}
